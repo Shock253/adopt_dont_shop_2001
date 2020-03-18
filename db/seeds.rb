439b1b303e288736c4df3dc2591c20c61a100a87
Ethan Hocking <eshocking253@gmail.com>
2020-03-18 16:17:40 -0600

    Add seed shelter data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+shelters = Shelter.create(
+                      [{
+                        name: "MaxFund Dog Shelter",
+                        address: "1005 Galapago St",
+                        city: "Denver",
+                        state: "CO",
+                        zip: "80204"
+                      },{
+                        name: "Denver Animal Shelter",
+                        address: "1241 W Bayaud Ave",
+                        city: "Denver",
+                        state: "CO",
+                        zip: "80223"
+                      }])
