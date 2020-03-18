3887743ed7be31ebc1986fc83ca8f85f0274bff7
Ethan Hocking <eshocking253@gmail.com>
2020-03-18 16:28:54 -0600

    Add route and page for showing a shelter

--- spec/features/user_can_see_all_shelters_spec.rb
+++ spec/features/user_can_see_all_shelters_spec.rb
@@ -22,4 +22,37 @@ RSpec.describe "as a visitor", type: :feature do
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_content(shelter_2.name)
   end
+
+  it "can visit an indexed shelter and see shelter data" do
+    shelter_1 = Shelter.create(
+                          name: "MaxFund Dog Shelter",
+                          address: "1005 Galapago St",
+                          city: "Denver",
+                          state: "CO",
+                          zip: "80204"
+                        )
+    shelter_2 = Shelter.create(
+                          name: "Denver Animal Shelter",
+                          address: "1241 W Bayaud Ave",
+                          city: "Denver",
+                          state: "CO",
+                          zip: "80223"
+                        )
+
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_1.address)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to have_content(shelter_1.state)
+    expect(page).to have_content(shelter_1.zip)
+
+    visit "/shelters/#{shelter_2.id}"
+
+    expect(page).to have_content(shelter_2.name)
+    expect(page).to have_content(shelter_2.address)
+    expect(page).to have_content(shelter_2.city)
+    expect(page).to have_content(shelter_2.state)
+    expect(page).to have_content(shelter_2.zip)
+  end
 end
