7098179065dd64af54ce5a58e7927d084cfcdb3e
Ethan Hocking <eshocking253@gmail.com>
2020-03-19 20:51:53 -0600

    Renamed shelters spec file

--- spec/features/shelters_spec.rb
+++ spec/features/shelters_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "as a visitor", type: :feature do
-  it "can visit the shelter index and show all shelter names" do
+RSpec.describe "as a visitor, when I visit the /shelters page", type: :feature do
+  it "then I can visit the shelter index and show all shelter names" do
     shelter_1 = Shelter.create(
                           name: "MaxFund Dog Shelter",
                           address: "1005 Galapago St",
@@ -56,3 +56,21 @@ RSpec.describe "as a visitor", type: :feature do
     expect(page).to have_content(shelter_2.zip)
   end
 end
+
+# User Story 4, Shelter Creation
+#
+# As a visitor
+# When I visit the Shelter Index page
+# Then I see a link to create a new Shelter, "New Shelter"
+# When I click this link
+# Then I am taken to '/shelters/new' where I  see a form for a new shelter
+# When I fill out the form with a new shelter's:
+# - name
+# - address
+# - city
+# - state
+# - zip
+# And I click the button "Create Shelter" to submit the form
+# Then a `POST` request is sent to '/shelters',
+# a new shelter is created,
+# and I am redirected to the Shelter Index page where I see the new Shelter listed.
