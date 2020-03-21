3124fe3a48c02204740f5d8f6690a04fd8d3752a
Ethan Hocking <eshocking253@gmail.com>
2020-03-21 16:08:43 -0600

    Add action and html for shelter index of pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,4 +2,8 @@ class PetsController < ApplicationController
    def index
      @shelters = Shelter.all
    end
+
+   def shelter_index
+     @shelter = Shelter.find(params[:shelter_id])
+   end
 end
