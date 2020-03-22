0f755a1814702df9be8e5a72d35d2767d2e722b8
Ethan Hocking <eshocking253@gmail.com>
2020-03-22 10:14:00 -0600

    Add spec for user story 11

--- spec/features/pets_spec.rb
+++ spec/features/pets_spec.rb
@@ -105,24 +105,49 @@ RSpec.describe "As a visitor,", type: :feature do
     expect(page).to have_css("img[src='https://vetmed.illinois.edu/wp-content/uploads/2017/12/pc-keller-hedgehog.jpg']")
 
   end
+
+  it "when I visit a pet show page,
+  then I can go to an update page to update that pet" do
+    # using pet 2 specifically to test adoption status
+    visit "/pets/#{@pet_2.id}"
+
+    click_link 'Update Pet'
+
+    expect(current_path).to eq("/pets/#{@pet_2.id}/edit")
+
+    fill_in 'Image', with: "https://vetmed.illinois.edu/wp-content/uploads/2017/12/pc-keller-hedgehog.jpg"
+    fill_in 'Name', with: "Reginald"
+    fill_in 'Description', with: "Soft boi"
+    fill_in 'Age', with: 2
+    fill_in 'Sex', with: "Male"
+
+    click_button 'Update Pet'
+
+    expect(current_path).to eq("/pets/#{@pet_2.id}")
+
+    expect(page).to have_css("img[src='https://vetmed.illinois.edu/wp-content/uploads/2017/12/pc-keller-hedgehog.jpg']")
+    expect(page).to have_content("Reginald")
+    expect(page).to have_content("Soft boi")
+    expect(page).to have_content(2)
+    expect(page).to have_content("Male")
+    expect(page).to have_content('pending adoption')
+  end
 end
 
 
-# User Story 10, Shelter Pet Creation
+# User Story 11, Pet Update
 #
 # As a visitor
-# When I visit a Shelter Pets Index page
-# Then I see a link to add a new adoptable pet for that shelter "Create Pet"
+# When I visit a Pet Show page
+# Then I see a link to update that Pet "Update Pet"
 # When I click the link
-# I am taken to '/shelters/:shelter_id/pets/new' where I see a form to add a new adoptable pet
-# When I fill in the form with the pet's:
+# I am taken to '/pets/:id/edit' where I see a form to edit the pet's data including:
 # - image
 # - name
 # - description
 # - approximate age
-# - sex ('female' or 'male')
-# And I click the button "Create Pet"
-# Then a `POST` request is sent to '/shelters/:shelter_id/pets',
-# a new pet is created for that shelter,
-# that pet has a status of 'adoptable',
-# and I am redirected to the Shelter Pets Index page where I can see the new pet listed
+# - sex
+# When I click the button to submit the form "Update Pet"
+# Then a `PATCH` request is sent to '/pets/:id',
+# the pet's data is updated,
+# and I am redirected to the Pet Show page where I see the Pet's updated information
